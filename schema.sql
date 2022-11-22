fdabc56bbc35f7162e028b13b1f597e34edb4b3c
EdsonGP723 <edsongp1007@outlook.com>
2022-11-22 10:08:23 -0600

    Agregando esquema finalizado

--- schema.sql
+++ schema.sql
@@ -1,60 +1,73 @@
-CREATE DATABASE IF NOT EXISTS tienda;
+CREATE DATABASE IF NOT EXISTS Juegos_Adquiridos;
 
-USE tienda;
+USE Juegos_Adquiridos;
 
-CREATE TABLE paises(
-  pais_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Género(
+  genero_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(70) NOT NULL,
+  descripcion VARCHAR(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Juegos(
+  juego_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL,
-  dominio CHAR(2) NOT NULL
+  foto VARCHAR(255)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE productos(
-  producto_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(50) NOT NULL,
-  descripcion TEXT,
-  foto VARCHAR(255),
-  precio FLOAT NOT NULL,
-  cantidad INTEGER UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE clientes(
-  cliente_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(50) NOT NULL,
-  apellidos VARCHAR(50) NOT NULL,
-  telefono VARCHAR(10) UNIQUE NOT NULL,
-  email VARCHAR(50) UNIQUE NOT NULL,
-  direccion VARCHAR(100),
-  cp CHAR(5) NOT NULL,
-  ciudad VARCHAR(50),
-  pais INTEGER UNSIGNED,
-  FOREIGN KEY (pais)
-    REFERENCES paises(pais_id)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE ventas(
-  venta_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  cliente_id INTEGER UNSIGNED,
-  fecha DATETIME NOT NULL,
-  monto FLOAT NOT NULL,
-  FOREIGN KEY(cliente_id)
-    REFERENCES clientes(cliente_id)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE articulos_x_ventas(
-  articulo_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  venta_id INTEGER UNSIGNED,
-  producto_id INTEGER UNSIGNED,
-  cantidad INTEGER UNSIGNED,
-  FOREIGN KEY(venta_id)
-    REFERENCES ventas(venta_id)
+CREATE TABLE Juegos_x_Género(
+  juego_x_genero_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  juego_id INTEGER UNSIGNED,
+  genero_id INTEGER UNSIGNED,
+  FOREIGN KEY (juego_id)
+    REFERENCES Juegos(juego_id)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
-  FOREIGN KEY(producto_id)
-    REFERENCES productos(producto_id)
+  FOREIGN KEY (genero_id)
+    REFERENCES Género(genero_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Desarrolladora(
+  desarrolladora_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Desarrolladora_x_Juego(
+ desarrolladora_x_juego_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+ desarrolladora_id INTEGER UNSIGNED,
+ juego_id INTEGER UNSIGNED,
+  FOREIGN KEY(desarrolladora_id)
+    REFERENCES Desarrolladora(desarrolladora_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY(juego_id)
+    REFERENCES Juegos(juego_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Launcher(
+  launcher_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(70) NOT NULL,
+  fundación VARCHAR(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE Juegos_Adquiridos_x_Launcher(
+  juegos_adquiridos_x_launcher_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  juego_id INTEGER UNSIGNED,
+  genero_id INTEGER UNSIGNED,
+  desarrolladora_id INTEGER UNSIGNED,
+  FOREIGN KEY(juego_id)
+    REFERENCES Juegos(juego_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY(genero_id)
+    REFERENCES Género(genero_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY(desarrolladora_id)
+    REFERENCES Desarrolladora(desarrolladora_id)
     ON DELETE RESTRICT
     ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
